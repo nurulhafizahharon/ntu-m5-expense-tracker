d7a1ba8508ff177d8744c974b2728d9ee939524b
Vignesh Nagamuthu <vignesh.nagamuthu@rohde-schwarz.com>
2023-11-17 09:05:59 +0800

    enable to catergories

--- screens/AddCategoryScreen.js
+++ screens/AddCategoryScreen.js
@@ -1,21 +1,38 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import axios from "axios";
 
 export default function AddCategoryScreen() {
   const [category, setCategory] = useState(""); // Need to change to array
 
+  const addCatergory = async () => {
+    const data = {categoryName: category};
+    try {
+      const response = await axios.post(
+        `http://10.0.2.2:8080/users/1/categories`, data 
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log("Error posting data: ", error);
+    }
+  }
+
   return (
-    <View>
-      <TextInput
-        placeholder="Category"
-        value={category}
-        onChangeText={(text) => setCategory(text)}
-      />
-      <Button
-        title="Add Category"
-        // onPress={}
-      />
-    </View>
+    <>
+      <View>
+        <TextInput
+          placeholder="Category"
+          value={category}
+          onChangeText={(text) => setCategory(text)}
+        />
+
+        <Button
+          title="Add Category"
+          onPress={addCatergory}
+        />
+      </View>
+    </>
+    
   );
 }
 
